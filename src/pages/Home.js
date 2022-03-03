c8c7e80de3d7533bd1ece1e9e198e8b78e95f844
Nicolas Layne <nicl9923@gmail.com>
2022-03-02 21:13:25 -0600

    Removed comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,30 +44,3 @@ const Home = () => {
 }
 
 export default Home;
-
-
-/*
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-  const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      // ...
-    } else {
-      // User is signed out
-      // ...
-    }
-  });
-*/
-
-/*
-    const auth = getAuth();
-    signOut(auth).then(() => {
-    // Sign-out successful.
-    }).catch((error) => {
-    // An error happened.
-    });
-*/
