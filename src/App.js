ff59af2a99c456002bf4fcc17f226207ef360609
Nicolas Layne <nicl9923@gmail.com>
2022-05-29 13:43:17 -0500

    Redirecting routes not matched to homepage (fixes users not logged in not finding URLs that do exist when logged in)

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ const App = () => {
             }
 
             <Route path='/' element={<Home />} />
+            <Route element={<Home />} />
           </Routes>
         ) : (
           <NotLoggedIn auth={auth} />
