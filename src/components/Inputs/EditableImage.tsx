e0db966f3bfe6b019f3c0c1781720bd9352a1b3e
Nicolas Layne <nicl9923@gmail.com>
2022-09-24 11:08:09 -0500

    Make more profile/family fields editable, implement EditableImage, some refactors, etc

--- src/components/Inputs/EditableImage.tsx
+++ src/components/Inputs/EditableImage.tsx
@@ -1,7 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControlLabel,
+  IconButton,
+} from '@mui/material';
+import { DropzoneArea } from 'mui-file-dropzone';
+import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+import { Edit } from '@mui/icons-material';
 
-const EditableImage = () => {
-  return <div>TODO: Editable Image</div>;
+// TODO: On image hover -> grey background + Edit icon overlay on image (and cursor pointer)
+
+interface EditableImageProps {
+  curImgLink?: string;
+  updateCurImgLink: (newImgLink: string) => void;
+  imgPlaceholder: JSX.Element;
+  height: number;
+  width: number;
+}
+
+const EditableImage = ({ curImgLink, updateCurImgLink, imgPlaceholder, height, width }: EditableImageProps) => {
+  const [isHoveringImg, setIsHoveringImg] = useState(false);
+  const [isEditingPhoto, setIsEditingPhoto] = useState(false);
+  const [newImgFile, setNewImgFile] = useState<File | null>(null);
+  const [deleteExistingPhoto, setDeleteExistingPhoto] = useState(false);
+
+  const updateImg = () => {
+    if (deleteExistingPhoto || newImgFile) {
+      const storage = getStorage();
+
+      // Submit new picture to Storage -> get/save link -> remove old one
+      if (curImgLink) {
+        const oldImgRef = ref(storage, curImgLink);
+        deleteObject(oldImgRef);
+      }
+
+      if (newImgFile) {
+        const imgRef = ref(storage, uuidv4());
+        uploadBytes(imgRef, newImgFile).then((snapshot) => {
+          getDownloadURL(snapshot.ref).then((url) => {
+            updateCurImgLink(url);
+          });
+        });
+      } else {
+        updateCurImgLink('');
+      }
+    }
+  };
+
+  return (
+    <Box>
+      <IconButton
+        onClick={() => setIsEditingPhoto(true)}
+        sx={{ position: 'relative', borderRadius: '50%' }}
+        onMouseOver={() => setIsHoveringImg(true)}
+        onMouseOut={() => setIsHoveringImg(false)}
+      >
+        {isHoveringImg && (
+          <Edit
+            sx={{
+              position: 'absolute',
+              top: '27%',
+              left: '29%',
+              zIndex: 100,
+              color: '#eaeaea',
+              height: height / 2,
+              width: width / 2,
+            }}
+          />
+        )}
+        <Avatar src={curImgLink} sx={{ height, width }}>
+          {!curImgLink && imgPlaceholder}
+        </Avatar>
+      </IconButton>
+
+      <Dialog open={isEditingPhoto} onClose={() => setIsEditingPhoto(false)} fullWidth>
+        <DialogTitle>Update/Delete image</DialogTitle>
+
+        <DialogContent>
+          {!deleteExistingPhoto && (
+            <DropzoneArea
+              acceptedFiles={['image/jpeg', 'image/png']}
+              filesLimit={1}
+              onChange={(files) => setNewImgFile(files[0])}
+              fileObjects={[]}
+            />
+          )}
+          {curImgLink && (
+            <FormControlLabel
+              control={
+                <Checkbox checked={deleteExistingPhoto} onChange={() => setDeleteExistingPhoto(!deleteExistingPhoto)} />
+              }
+              label='Only delete existing photo'
+            />
+          )}
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={() => setIsEditingPhoto(false)}>Cancel</Button>
+          <Button variant='contained' onClick={updateImg} disabled={!newImgFile && !deleteExistingPhoto}>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 };
 
 export default EditableImage;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,54 +1,18 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
-import {
-  Avatar,
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Divider,
-  FormControlLabel,
-  IconButton,
-  InputLabel,
-  Paper,
-  Snackbar,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
-import { Add, Close, ContentCopyOutlined, Edit, Logout } from '@mui/icons-material';
-import MapPicker from 'react-google-map-picker';
-import { DropzoneArea } from 'mui-file-dropzone';
-import { v4 as uuidv4 } from 'uuid';
-import copy from 'clipboard-copy';
-import NoFamily from '../components/NoFamily';
+import React, { useContext } from 'react';
+import { Container, Paper, Stack, Typography } from '@mui/material';
 import { UserContext } from '../App';
 import { FirebaseContext } from '../Firebase';
 import NoProfile from 'components/NoProfile';
-import { GenericObject, UserProfile } from 'models/types';
+import { UserProfile } from 'models/types';
+import Family from 'components/Family';
+import EditableLabel from 'components/Inputs/EditableLabel';
+import EditableImage from 'components/Inputs/EditableImage';
 
 const Profile = () => {
   const firebase = useContext(FirebaseContext);
-  const { userId, profile, family, getProfile } = useContext(UserContext);
-  const [familyMemberProfiles, setFamilyMemberProfiles] = useState<UserProfile[]>([]);
-  const [copiedInviteLink, setCopiedInviteLink] = useState(false);
-  // Profile edit
-  const [editingProfile, setEditingProfile] = useState(false);
-  const [profileEditedName, setProfileEditedName] = useState<string | undefined>(
-    profile ? profile.firstName : undefined
-  );
-  const [profileEditedPhoto, setProfileEditedPhoto] = useState<File | null>(null);
-  const [deleteExistingPhoto, setDeleteExistingPhoto] = useState(false);
+  const { userId, profile, getProfile } = useContext(UserContext);
 
-  const [deletingFamily, setDeletingFamily] = useState(false);
-  const [leavingFamily, setLeavingFamily] = useState(false);
-
-  const mergeProfileProperty = (profObjToMerge: GenericObject, profileId = userId, refreshProfile = true) => {
+  const mergeProfileProperty = (profObjToMerge: Partial<UserProfile>, profileId = userId, refreshProfile = true) => {
     if (!profileId) return;
 
     firebase.updateProfile(profileId, profObjToMerge).then(() => {
@@ -56,147 +20,15 @@ const Profile = () => {
     });
   };
 
-  const getFamilyMemberProfiles = () => {
-    if (!family?.members) return;
+  const updateProfileName = (newName?: string) => {
+    if (!newName) return;
 
-    const famMemProfs: UserProfile[] = [];
-
-    family.members.forEach((member) => {
-      if (member === userId) return;
-
-      firebase.getProfile(member).then((doc) => {
-        if (doc.exists()) {
-          const profileData = doc.data() as UserProfile;
-          famMemProfs.push(profileData);
-          setFamilyMemberProfiles(famMemProfs);
-        } else {
-          // Specified family member doesn't exist
-        }
-      });
-    });
+    mergeProfileProperty({ firstName: newName });
   };
 
-  const saveEditedProfile = () => {
-    if (!profile) return;
-
-    if (deleteExistingPhoto || profileEditedPhoto) {
-      const storage = getStorage();
-
-      // Submit new profile picture to Storage and get/save link, and remove old one
-      if (profile.imgLink) {
-        const oldImgRef = ref(storage, profile.imgLink);
-        deleteObject(oldImgRef);
-      }
-
-      if (profileEditedPhoto) {
-        const imgRef = ref(storage, uuidv4());
-        uploadBytes(imgRef, profileEditedPhoto).then((snapshot) => {
-          getDownloadURL(snapshot.ref).then((url) => {
-            mergeProfileProperty({ imgLink: url });
-          });
-        });
-      } else {
-        mergeProfileProperty({ imgLink: '' });
-      }
-    }
-
-    // Submit new profile name
-    if (profileEditedName) {
-      mergeProfileProperty({ firstName: profileEditedName });
-    }
-
-    setProfileEditedName(profile.firstName);
-    setProfileEditedPhoto(null);
-    setEditingProfile(false);
-    setDeleteExistingPhoto(false);
+  const updateProfileImgLink = (newImgLink: string) => {
+    mergeProfileProperty({ imgLink: newImgLink });
   };
-
-  const deleteFamily = () => {
-    if (!profile || !family) return;
-
-    // Set each profile in family.members familyId property to ''
-    if (family.members) {
-      family.members.forEach((member) => {
-        mergeProfileProperty({ familyId: '' }, member, false);
-      });
-    }
-
-    // Delete residences and vehicles
-    if (family.residences) {
-      family.residences.forEach((res) => {
-        firebase.deleteResidence(res);
-      });
-    }
-
-    if (family.vehicles) {
-      family.vehicles.forEach((veh) => {
-        firebase.deleteVehicle(veh);
-      });
-    }
-
-    // Delete family doc
-    firebase.deleteFamily(profile.familyId).then(() => {
-      mergeProfileProperty({ familyId: '' });
-    });
-  };
-
-  const leaveFamily = () => {
-    if (!profile || !family) return;
-
-    const newMembers = [...family.members];
-    const curUserIdx = newMembers.findIndex((mem) => mem === userId);
-    newMembers.splice(curUserIdx, 1);
-
-    // Make first member in family new headOfFamily (if curUser is hoF)
-    const mergeFam = { members: newMembers, headOfFamily: family.headOfFamily };
-    if (family.headOfFamily === userId) {
-      mergeFam.headOfFamily = newMembers[0];
-    }
-
-    firebase.updateFamily(profile.familyId, mergeFam);
-    mergeProfileProperty({ familyId: '' });
-  };
-
-  const copyInviteLink = () => {
-    if (!profile) return;
-
-    copy(`https://our-home-239c1.firebaseapp.com/joinFamily/${profile.familyId}`);
-    setCopiedInviteLink(true);
-  };
-
-  const exportFamilyDataJSON = () => {
-    if (!family) return;
-
-    const fileName = 'FamilyData';
-    const familyData = {
-      name: family.name,
-      members: family.members,
-      pets: family.pets,
-      boardMarkdown: family.boardMarkdown,
-      location: family.location,
-      apiKeys: {
-        openWeather: family.openweathermap_api_key,
-        gmaps: family.gmaps_api_key,
-      },
-    };
-
-    const json = JSON.stringify(familyData, null, 2);
-    const blob = new Blob([json], { type: 'application/json' });
-    const href = URL.createObjectURL(blob);
-
-    const link = document.createElement('a');
-    link.href = href;
-    link.download = fileName + '.json';
-    document.body.appendChild(link);
-    link.click();
-
-    document.body.removeChild(link);
-    URL.revokeObjectURL(href);
-  };
-
-  useEffect(() => {
-    if (family) getFamilyMemberProfiles();
-  }, [family]);
 
   return (
     <Container maxWidth='md'>
@@ -207,268 +39,29 @@ const Profile = () => {
           <Typography variant='h2'>My Profile</Typography>
 
           <Stack alignItems='center' justifyContent='center'>
-            <Avatar src={profile.imgLink ? profile.imgLink : undefined} alt='profile' sx={{ width: 164, height: 164 }}>
-              {profile.imgLink ? null : <Typography variant='h1'>{profile.firstName[0].toUpperCase()}</Typography>}
-            </Avatar>
+            <EditableImage
+              curImgLink={profile.imgLink}
+              updateCurImgLink={updateProfileImgLink}
+              imgPlaceholder={<Typography variant='h1'>{profile.firstName[0].toUpperCase()}</Typography>}
+              height={164}
+              width={164}
+            />
 
             <Typography variant='h5' mt={1} mb={2}>
               {profile.firstName}
             </Typography>
 
-            <Button variant='outlined' startIcon={<Edit />} onClick={() => setEditingProfile(true)}>
-              Edit Profile
-            </Button>
-
-            <Dialog open={editingProfile} onClose={() => setEditingProfile(false)} fullWidth>
-              <DialogTitle>Edit Profile</DialogTitle>
-
-              <DialogContent>
-                <TextField
-                  autoFocus
-                  variant='standard'
-                  label='First Name'
-                  value={profileEditedName}
-                  onChange={(event) => setProfileEditedName(event.target.value)}
-                />
-
-                <InputLabel sx={{ mt: 4, mb: 1 }}>Profile Photo</InputLabel>
-                {!deleteExistingPhoto && (
-                  <DropzoneArea
-                    acceptedFiles={['image/jpeg', 'image/png']}
-                    filesLimit={1}
-                    onChange={(files) => setProfileEditedPhoto(files[0])}
-                    fileObjects={[]}
-                  />
-                )}
-                {profile.imgLink && (
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={deleteExistingPhoto}
-                        onChange={() => setDeleteExistingPhoto(!deleteExistingPhoto)}
-                      />
-                    }
-                    label='Delete existing photo'
-                  />
-                )}
-              </DialogContent>
-
-              <DialogActions>
-                <Button onClick={() => setEditingProfile(false)}>Cancel</Button>
-                <Button variant='contained' onClick={saveEditedProfile}>
-                  Save
-                </Button>
-              </DialogActions>
-            </Dialog>
+            <EditableLabel
+              text={profile.firstName}
+              fieldName='First Name'
+              fieldType='EntityName'
+              onSubmitValue={updateProfileName}
+            />
           </Stack>
         </Paper>
       )}
 
-      {!family ? (
-        <NoFamily />
-      ) : (
-        <Paper sx={{ p: 3 }}>
-          <Typography variant='h3'>My Family</Typography>
-
-          <Box mt={1} mb={4}>
-            <Stack direction='row' alignItems='center'>
-              <Typography variant='h5'>Family Name</Typography>
-              {userId === family.headOfFamily && (
-                <IconButton>
-                  <Edit />
-                </IconButton>
-              )}
-            </Stack>
-
-            <Typography variant='h6'>{family.name}</Typography>
-          </Box>
-
-          <Box mb={4}>
-            <Typography variant='h5'>Members</Typography>
-            <Stack direction='row' mb={3}>
-              {familyMemberProfiles &&
-                familyMemberProfiles.map((prof: UserProfile) => (
-                  <Stack key={prof.firstName} alignItems='center' justifyContent='center'>
-                    <Typography variant='h6'>{prof.firstName}</Typography>
-                    <Avatar
-                      src={prof.imgLink ? prof.imgLink : undefined}
-                      alt='family member'
-                      sx={{ width: 128, height: 128 }}
-                    >
-                      {prof.imgLink ? null : prof.firstName[0].toUpperCase()}
-                    </Avatar>
-                    {userId === family.headOfFamily && (
-                      <Button variant='outlined' startIcon={<Close />}>
-                        Remove
-                      </Button>
-                    )}
-                  </Stack>
-                ))}
-            </Stack>
-            {userId === family.headOfFamily && (
-              <Box width='100%' mx='auto'>
-                <Button variant='contained' startIcon={<ContentCopyOutlined />} onClick={copyInviteLink}>
-                  Copy family invite link
-                </Button>
-                <Snackbar
-                  open={copiedInviteLink}
-                  autoHideDuration={2000}
-                  onClose={() => setCopiedInviteLink(false)}
-                  message='Copied invite link'
-                />
-              </Box>
-            )}
-          </Box>
-
-          <Box>
-            <Typography variant='h5'>Pets</Typography>
-            <Stack direction='row' mb={3}>
-              {family.pets &&
-                family.pets.map((pet) => (
-                  <Stack key={pet.name} alignItems='center' justifyContent='center'>
-                    <Typography variant='body1'>{pet.name}</Typography>
-                    <Avatar src={pet.imgLink ? pet.imgLink : undefined} alt='pet' sx={{ width: 96, height: 96 }}>
-                      {pet.imgLink ? null : pet.name[0].toUpperCase()}
-                    </Avatar>
-                    {userId === family.headOfFamily && (
-                      <Box>
-                        <Button variant='outlined' startIcon={<Edit />}>
-                          Edit
-                        </Button>
-                        <Button variant='outlined' startIcon={<Close />}>
-                          Remove
-                        </Button>
-                      </Box>
-                    )}
-                  </Stack>
-                ))}
-            </Stack>
-            {userId === family.headOfFamily && (
-              <Button variant='contained' startIcon={<Add />}>
-                Add a pet
-              </Button>
-            )}
-          </Box>
-
-          {userId === family.headOfFamily && (
-            <Box mt={4}>
-              <Divider />
-              <Typography variant='h5' mt={2}>
-                Weather Applet Information
-              </Typography>
-
-              <Stack mt={1} mb={2}>
-                <Stack direction='row' alignItems='center'>
-                  <Typography variant='h6'>Google Maps API Key</Typography>
-                  <IconButton>
-                    <Edit />
-                  </IconButton>
-                </Stack>
-
-                <Typography variant='body1'>
-                  {family.gmaps_api_key
-                    ? family.gmaps_api_key
-                    : `Obtain and input a Google Maps API key if you would like
-                    to use the built-in location picker, otherwise manually find/input
-                    your location's coordinates`}
-                </Typography>
-              </Stack>
-
-              <Box mb={2}>
-                <Stack direction='row' alignItems='center'>
-                  <Typography variant='h6'>OpenWeatherMap API Key</Typography>
-                  <IconButton>
-                    <Edit />
-                  </IconButton>
-                </Stack>
-
-                <Typography variant='body1'>
-                  {family.openweathermap_api_key
-                    ? family.openweathermap_api_key
-                    : `Obtain and input an OpenWeatherMap 'Current Weather' API key,
-                    and set your family's location below, if you would like to see
-                    your local weather forecast on the homepage`}
-                </Typography>
-              </Box>
-
-              <Box mb={6}>
-                <Stack direction='row' alignItems='center'>
-                  <Typography variant='h6'>Location</Typography>
-                  <IconButton>
-                    <Edit />
-                  </IconButton>
-                </Stack>
-                {family.location && (
-                  <Box>
-                    <Box>
-                      <Typography variant='body1'>Latitude: {family.location.lat}</Typography>
-                      <Typography variant='body1'>Longitude: {family.location.long}</Typography>
-                    </Box>
-                    {family.gmaps_api_key && (
-                      <MapPicker
-                        defaultLocation={{
-                          lat: parseFloat(family.location.lat),
-                          lng: parseFloat(family.location.long),
-                        }}
-                        style={{ height: 500, width: 750 }}
-                        onChangeLocation={(newLat, newLong) => {
-                          console.log(newLat + ' ' + newLong);
-                        }}
-                        apiKey={family.gmaps_api_key}
-                      />
-                    )}
-                  </Box>
-                )}
-              </Box>
-            </Box>
-          )}
-
-          {userId === family.headOfFamily ? (
-            <Stack spacing={5}>
-              <Button variant='contained' onClick={exportFamilyDataJSON}>
-                Export family data
-              </Button>
-              <Button variant='contained' color='error' startIcon={<Close />} onClick={() => setDeletingFamily(true)}>
-                Delete Family
-              </Button>
-            </Stack>
-          ) : (
-            <Button variant='contained' color='error' startIcon={<Logout />} onClick={() => setLeavingFamily(true)}>
-              Leave Family
-            </Button>
-          )}
-
-          <Dialog open={deletingFamily} onClose={() => setDeletingFamily(false)} fullWidth>
-            <DialogTitle>Delete family?</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Are you sure you want to delete the {family.name} family?</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button variant='text' onClick={() => setDeletingFamily(false)}>
-                Cancel
-              </Button>
-              <Button variant='contained' onClick={deleteFamily}>
-                Delete
-              </Button>
-            </DialogActions>
-          </Dialog>
-
-          <Dialog open={leavingFamily} onClose={() => setLeavingFamily(false)} fullWidth>
-            <DialogTitle>Leave family?</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Are you sure you want to leave the {family.name} family?</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button variant='text' onClick={() => setLeavingFamily(false)}>
-                Cancel
-              </Button>
-              <Button variant='contained' onClick={leaveFamily}>
-                Leave
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Paper>
-      )}
+      <Family mergeProfileProperty={mergeProfileProperty} />
     </Container>
   );
 };
