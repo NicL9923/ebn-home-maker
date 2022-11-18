b78b9eac85b5c349f17c10253a018373ee1b0d98
Nicolas Layne <nicl9923@gmail.com>
2022-11-17 20:40:52 -0600

    Convert to the actual react-table (tanstack) ... much more pleasurable experience!

--- src/components/Finances/Transactions.tsx
+++ src/components/Finances/Transactions.tsx
@@ -1,103 +1,133 @@
-import React, { useState } from 'react';
-import { IBudget } from 'models/types';
+import React, { useMemo, useState } from 'react';
+import { IBudget, Transaction } from 'models/types';
 import AddTransaction from 'components/Forms/AddTransaction';
 import { useUserStore } from 'state/UserStore';
 import { useFirestoreDocumentMutation } from '@react-query-firebase/firestore';
 import { doc } from 'firebase/firestore';
 import { db, FsCol } from '../../firebase';
 import { MdAdd, MdDelete } from 'react-icons/md';
-import { Box, Button, Heading, Stack } from '@chakra-ui/react';
-import { Column, CompactTable } from '@table-library/react-table-library/compact';
-import { usePagination } from '@table-library/react-table-library/pagination';
-import { useSort } from '@table-library/react-table-library/sort';
-import { useRowSelect } from '@table-library/react-table-library/select';
+import { Box, Button, Checkbox, Heading, Stack, Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  RowSelectionState,
+  useReactTable,
+} from '@tanstack/react-table';
 
-const transactionTableColumns: Column[] = [
-  {
-    label: 'Amount',
-    renderCell: (item) =>
-      `$${item.amt.toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      })}`,
-  },
-  {
-    label: 'Description',
-    renderCell: (item) => item.name,
-  },
-  {
-    label: 'Subcategory',
-    renderCell: (item) => item.subcategory,
-    // TODO: Make a custom (or re-use the existing) edit component for this (same Select/Switch component as Dialog)
-  },
-  {
-    label: 'Date',
-    renderCell: (item) =>
-      new Date(item.timestamp).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
-  },
-];
+const rowsPerPageOptions = [10, 25, 50, 100];
 
-interface PageState {
-  page: number;
-  size: number;
+interface TableTransaction extends Omit<Transaction, 'timestamp'> {
+  timestamp: Date;
 }
 
 interface TransactionsProps {
   budget: IBudget;
 }
 
-const Transactions = ({ budget }: TransactionsProps): JSX.Element => {
+const Transactions = ({ budget }: TransactionsProps) => {
   const family = useUserStore((state) => state.family);
 
-  const transactionTableData = {
-    nodes: budget.transactions.map((transaction) => ({ ...transaction, id: transaction.id?.toString() ?? '' })),
-  };
-
   const [addingTransaction, setAddingTransaction] = useState(false);
-  const [selection, setSelection] = useState<string[]>([]);
-  // TODO: rowsPerPageOptions={[10, 20, 50, 100]}
-  const [pageState, setPageState] = useState<PageState>({ page: 0, size: 20 });
-
-  const sort = useSort(
-    transactionTableData,
-    {
-      state: {
-        sortKey: 'TIMESTAMP',
-        reverse: true,
-      },
-    },
-    {
-      sortFns: {
-        TIMESTAMP: (array) => array.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()),
-      },
-    }
-  );
-
-  const pagination = usePagination(transactionTableData, {
-    state: pageState,
-    onChange: (_action, state) => setPageState(state as PageState),
-  });
-
-  const select = useRowSelect(transactionTableData, {
-    state: {
-      ids: selection,
-    },
-    onChange: (_action, state) => setSelection(state.ids),
-  });
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+  // TODO: editable fields
 
   const budgetDocMutation = useFirestoreDocumentMutation(doc(db, FsCol.Budgets, family?.budgetId ?? 'undefined'), {
     merge: true,
   });
 
+  const transactionColumns = useMemo<ColumnDef<TableTransaction>[]>(
+    () => [
+      {
+        id: 'select',
+        header: ({ table }) => (
+          <Checkbox
+            isChecked={table.getIsAllRowsSelected()}
+            isIndeterminate={table.getIsSomeRowsSelected()}
+            onChange={table.getToggleAllRowsSelectedHandler()}
+          />
+        ),
+        cell: ({ row }) => (
+          <div>
+            <Checkbox
+              isChecked={row.getIsSelected()}
+              //isIndeterminate={row.getIsSomeSelected()}
+              onChange={row.getToggleSelectedHandler()}
+            />
+          </div>
+        ),
+      },
+      {
+        accessorKey: 'amt',
+        header: 'Amount',
+        cell: (info) =>
+          `$${info.getValue<number>().toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}`,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Description',
+        cell: (info) => info.getValue<string>(),
+      },
+      {
+        accessorKey: 'subcategory',
+        header: 'Subcategory',
+        cell: (info) => info.getValue<string>(),
+        // TODO: Make a custom (or re-use the existing) edit component for this (same Select/Switch component as Dialog)
+      },
+      {
+        accessorKey: 'timestamp',
+        header: 'Date',
+        cell: (info) =>
+          info.getValue<Date>().toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }),
+      },
+    ],
+    []
+  );
+
+  const transactionData = useMemo<TableTransaction[]>(
+    () => budget.transactions.map((t) => ({ ...t, timestamp: new Date(t.timestamp) })),
+    [budget.transactions]
+  );
+
+  const table = useReactTable({
+    columns: transactionColumns,
+    data: transactionData,
+    initialState: {
+      pagination: {
+        pageIndex: 0,
+        pageSize: rowsPerPageOptions[0],
+      },
+      sorting: [{ id: 'timestamp', desc: true }],
+    },
+    state: {
+      rowSelection,
+    },
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
   const removeTransactions = () => {
     if (!family?.budgetId) return;
 
+    if (!window.confirm('Are you sure you want to delete the selected transactions?')) return;
+
     let updArr = [...budget.transactions];
 
-    updArr = updArr.filter((_val, idx) => selection.indexOf(idx.toString()) === -1); // Efficient way to remove transaction(s) from array
+    updArr = updArr.filter((_val, idx) => !rowSelection[idx]);
 
     budgetDocMutation.mutate({ transactions: updArr });
-    setSelection([]);
+    setRowSelection({});
   };
 
   /* TODO: Hook this back up
@@ -117,43 +147,96 @@ const Transactions = ({ budget }: TransactionsProps): JSX.Element => {
   return (
     <Box mt={2} ml={1} mr={1}>
       <Heading mb={2}>Transactions</Heading>
-      <Button leftIcon={<MdAdd />} onClick={() => setAddingTransaction(true)}>
-        Add transaction
-      </Button>
-      <Stack height={500} mt={3} mb={2}>
-        <CompactTable
-          columns={transactionTableColumns}
-          data={transactionTableData}
-          pagination={pagination}
-          sort={sort}
-          select={select}
-        />
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <span>Total Pages: {pagination.state.getTotalPages(transactionTableData.nodes)}</span>
 
-          <span>
-            Page:{' '}
-            {pagination.state.getPages(transactionTableData.nodes).map((_: unknown, pageIndex: number) => (
-              <button
-                key={pageIndex}
-                type='button'
-                style={{
-                  fontWeight: pagination.state.page === pageIndex ? 'bold' : 'normal',
-                }}
-                onClick={() => pagination.fns.onSetPage(pageIndex)}
-              >
-                {pageIndex + 1}
-              </button>
-            ))}
-          </span>
-        </div>
+      <Stack direction='row' mb={2} justifyContent='center'>
+        <Button leftIcon={<MdAdd />} onClick={() => setAddingTransaction(true)}>
+          Add transaction
+        </Button>
+
+        <Button
+          leftIcon={<MdDelete />}
+          onClick={removeTransactions}
+          colorScheme='red'
+          display={Object.keys(rowSelection).length > 0 ? undefined : 'none'}
+        >
+          Remove transaction{Object.keys(rowSelection).length > 1 && 's'}
+        </Button>
       </Stack>
 
-      {selection.length > 0 && (
-        <Button leftIcon={<MdDelete />} color='error' onClick={removeTransactions} sx={{ mb: 3 }}>
-          Remove transaction{selection.length > 1 && 's'}
-        </Button>
-      )}
+      <Stack mt={3} mb={2}>
+        <Table>
+          <Thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <Tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <Th key={header.id} colSpan={header.colSpan}>
+                    {!header.isPlaceholder && (
+                      <div
+                        className={header.column.getCanSort() ? 'cursor-pointer select-none' : ''}
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                        {{
+                          asc: ' (by Farthest)',
+                          desc: ' (by Nearest)',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </Th>
+                ))}
+              </Tr>
+            ))}
+          </Thead>
+
+          <Tbody>
+            {table.getRowModel().rows.map((row) => (
+              <Tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                ))}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+
+        <Box>
+          <Button onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
+            {'<<'}
+          </Button>
+
+          <Button onClick={table.previousPage} disabled={!table.getCanPreviousPage()}>
+            {'<'}
+          </Button>
+
+          <Button onClick={table.nextPage} disabled={!table.getCanNextPage()}>
+            {'>'}
+          </Button>
+
+          <Button onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
+            {'>>'}
+          </Button>
+
+          <span>
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+            </strong>
+          </span>
+
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {rowsPerPageOptions.map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </Box>
+      </Stack>
 
       <AddTransaction isOpen={addingTransaction} setIsOpen={setAddingTransaction} budget={budget} />
     </Box>
--- src/models/types.d.ts
+++ src/models/types.d.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export type GenericObject<T = any> = { [key: string]: T };
 
 // ID === user.uid
